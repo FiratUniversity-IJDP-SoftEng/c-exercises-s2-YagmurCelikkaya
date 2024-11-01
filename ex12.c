10deec32963560096d971b57e5bd7f64f4fc9c07
YagmurCelikkaya <ry.celikkaya@gmail.com>
2024-11-01 22:42:23 +0300

    Update ex12.c

--- ex12.c
+++ ex12.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Please enter a number less than 50: ");
+    scanf("%d" , &n);
 
+    if(n < 0 || n >= 50)
+    {
+        printf("Please be careful , print a number between 0 and 50.");
+        return 1;
+    }
+    long long factorial = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        factorial *= i;
+    }
+        printf("Factorial of %d is %lld\n", n , factorial);
+}
