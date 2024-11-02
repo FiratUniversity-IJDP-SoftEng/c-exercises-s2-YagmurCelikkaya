29d3e7ab42375c39d6138cabef3764ba1fbf3902
YagmurCelikkaya <ry.celikkaya@gmail.com>
2024-11-02 17:46:23 +0300

    Update ex07.c

--- ex07.c
+++ ex07.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    printf("i*5\t100 - ((i-1)*10+1)\ti\n");
+    printf("__________________________________\n\n");
 
+    for (i = 1; i <= 10; i++)
+    {
+        int column1 = i * 5;
+        int column2 = 100 - ((i - 1) * 10 + 1);
+        int column3 = i;
+
+        printf("%d\t|\t%d\t|\t%d\n", column1, column2, column3);
+    }
+
+}
