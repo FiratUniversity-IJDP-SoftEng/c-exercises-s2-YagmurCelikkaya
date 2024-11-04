eaf7db4f60cc8ffcaf1c1147fdfbb79a960c5598
YagmurCelikkaya <ry.celikkaya@gmail.com>
2024-11-04 20:08:25 +0300

    Update ex15.c

--- ex15.c
+++ ex15.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+int main() 
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 5 - i; j++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        for (j = i - 1; j >= 1; j--)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    for (i = 4; i >= 1; i--)
+    {
+        for (j = 1; j <= 5 - i; j++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        for (j = i - 1; j >= 1; j--)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+}
+
+
+
+
+
+
+
+
+
 
